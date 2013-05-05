4cc2c0ca10e27c2e99fdc7d5ae1ecee423e3ce48
GreenYoshi <philip.whitehall@xiinet.com>
2013-05-05 14:30:59 +0100

    Added test page. Ensured the class was properly structured. It was not before. Woops.

--- XeForm.php
+++ XeForm.php
@@ -9,32 +9,42 @@
  * @author Philip Whitehall <philip.whitehall@xiinet.com> 
  * @copyright Copyright 2006-2013 Xiin Networks <http://xiinet.com/>
  * @link http://xiinengine.com/
- * @since v2.0
+ * @version v1.0
  */
+class XeForm {
 
-class XiinEngine_Form {
-	private $db, $action_location, $action_type, $markup_type;
+    private $db, $action_location, $action_type, $markup_type, $errors;
 
-	public __construct($db, $action_location, $action_type, $markup_type) {
-		$this->db = strip_tags($db);
-		$this->action_location = $action_location;
-		$this->action_type = $action_type;
-		$this->markup_type = $markup_type;
-	}
+    public function __construct($db, $action_location, $action_type, $markup_type) {
+        $this->db = strip_tags($db);
+        $this->action_location = $action_location;
+        $this->action_type = (strtolower($action_type) == "post" || strtolower($action_type) == "get") ?
+                (strtolower($action_type) == 'post') ? 'post' : 'get'  :
+                $this->xe_error('invalid_action_type');
+        $this->markup_type = (strtolower($markup_type) == "post" || strtolower($markup_type) == "get") ?
+                (strtolower($markup_type) == 'ulli') ? 'ulli' : 'table' :
+                $this->xe_error('invalid_markup_type');
+        
+        if (!empty($this->errors)) echo $this->errors;
+        echo $db.$action_location. $action_type.$markup_type;
+    }
 
-	public __destruct() {
+    public function __destruct() {
+        
+    }
 
-	}
+    /**
+     * 	Localised XeError function for standalone version
+     */
+    public function xe_error($error_type) {
+        $error_labels = array(
+            'invalid_action_type' => 'There was a problem with your action type variable in XeForm.',
+            'invalid_markup_type' => 'There was a problem with your markup type variable in XeForm.'
+        );
+        
+        (empty($this->errors)) ? $this->errors = $error_labels[$error_type] : $this->errors .= '<br />'.$error_labels[$error_type];
+    }
 
-	/**
-	*	Localised XeError function for standalone version
-	*/
-	public xe_error() {
-
-	}
 }
-
-
-
 
 ?>
---
+++ test/index.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * XiinEngine
+ *
+ * XiinEngine and its libraries are supplied under the MIT license. Please read license.md  in the root directory
+ *
+ * @package XiinEngine Standalone Libraries
+ * @author Philip Whitehall <philip.whitehall@xiinet.com> 
+ * @copyright Copyright 2006-2013 Xiin Networks <http://xiinet.com/>
+ * @link http://xiinengine.com/
+ * @version v1.0
+ */
+
+include ('../XeForm.php');
+
+new XeForm('db', 'edit.php', 'POST', 'ulli');
+
+?>
